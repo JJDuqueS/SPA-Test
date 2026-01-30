c636b9d68beff0af5ebce9e5e9bb3842505ed8ea
jduque <jduqueserna@gmail.com>
2026-01-30 14:26:51 -0500

    feat: configure dotenv and add package.json with scripts for concurrent execution

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,9 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+import path from "node:path";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+
+dotenv.config({ path: path.resolve(__dirname, "../../..", ".env") });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
